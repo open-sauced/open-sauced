23df0e70f42efe5bc5ee33da5a565d50750e1b07
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-10-15 12:39:23 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Open Sauced</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
