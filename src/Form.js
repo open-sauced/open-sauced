21b1e61c90db9d3166e109043f208e0d9b95d242
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-10-19 06:23:08 -0700

    slight styling

--- src/Form.js
+++ src/Form.js
@@ -5,28 +5,40 @@ import './button.css';
 class Form extends Component {
   constructor(props) {
     super(props)
-    this.state = {data: null}
+    this.setUrl = this.setUrl.bind(this);
+    this.fetchRepoData = this.fetchRepoData.bind(this);
+    this.state = {
+      uri: null,
+      repo: null,
+      name: null,
+      data: {}
+    };
   }
 
-  componentDidMount() {
-    this.fetchRepoData("yarnpkg", "yarn")
+  setUrl(e) {
+    this.setState({url: e.target.value})
   }
 
-  fetchRepoData(name, repo) {
-    api.fetchRepositoryData(name, repo).then(
-      (response) => this.setState({data: response.data})
+  fetchRepoData() {
+    const url = this.state.url.split("/")
+    api.fetchRepositoryData(url[3], url[4]).then(
+      (response) => this.setState({data: response.data.data.repositoryOwner.repository})
     );
   }
 
   render() {
+    console.log(this.state.data)
+    const {name, url, description, forks, owner, stargazers, issues} = this.state.data
     return (
       <div className="Form grid">
         <h2 className="title">Enter a GitHub URL</h2>
-        <div className="form-container shadow">
-          <form name="form user-content-submission" action="thank-you" netlify>
-            <input placeholder="https://github.com/netlify/netlify-cms"/>
-            <button className="">Scrape</button>
-          </form>
+        <div className="">
+          <div name="">
+            <input className="urlForm" onChange={this.setUrl} value={this.state.url} placeholder="https://github.com/netlify/netlify-cms"/>
+            <p>
+              <button className="button-ui-default" onClick={this.fetchRepoData}>Scrape</button>
+            </p>
+          </div>
           <h3>
             We are only looking for open source repos that meet one or more of the following criteria:
           </h3>
@@ -40,34 +52,34 @@ class Form extends Component {
           <form name="grid-full form user-content-submission" action="thank-you" netlify>
             <p>
               <label>Name: </label>
-              <input className="support-input-form" type="text" name="sitename" required />
+              <input className="support-input-form" value={name} type="text" name="sitename" required />
             </p>
             <p>
               <label>Link: </label>
-              <input className="boxed-input light-shadow" type="text" name="contentlink" required />
+              <input className="boxed-input light-shadow" value={url} type="url" name="contentlink" required />
             </p>
             <p>
               <label>Owner: </label>
-              <input className="boxed-input light-shadow" type="email" name="contactemail" required />
+              <input className="boxed-input light-shadow" value={owner && owner.login} name="repoowner" required />
             </p>
             <p>
               <label>Contributors: </label>
-              <input className="boxed-input light-shadow" type="text" name="contributors" required />
+              <input className="boxed-input light-shadow" type="text" name="contributors" />
             </p>
             <p>
               <label>Stars: </label>
-              <input className="boxed-input light-shadow" type="text" name="stars" required />
+              <input className="boxed-input light-shadow" value={stargazers && stargazers.totalCount} type="text" name="stars" required />
             </p>
             <p>
               <label>Forks: </label>
-              <input className="boxed-input light-shadow" type="text" name="forks" required />
+              <input className="boxed-input light-shadow" value={forks && forks.totalCount} type="text" name="forks" required />
             </p>
             <p>
               <label>Issues: </label>
-              <input className="boxed-input light-shadow" type="text" name="issues" required />
+              <input className="boxed-input light-shadow" value={issues && issues.totalCount} type="text" name="issues" required />
             </p>
             <p>
-              <textarea className="boxed-input text-box light-shadow" type="text" placeholder="Note about this repo" name="notes"></textarea>
+              <textarea className="boxed-input text-box light-shadow" value={description} type="text" placeholder="Note about this repo" name="notes"></textarea>
             </p>
             <p>
               <button className="button-ui-primary">Send</button>
