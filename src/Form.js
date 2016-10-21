fe0244987c59e29e6951d8d85a595a1fa15039b3
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-10-20 19:34:22 -0700

    fixes setstate

--- src/Form.js
+++ src/Form.js
@@ -18,40 +18,45 @@ class Form extends Component {
     this.handleContributorsChange = this.handleContributorsChange.bind(this);
     this.sendDataToFireBase = this.sendDataToFireBase.bind(this);
     this.state = {
-      uri: null,
-      repo: null,
-      name: null,
+      uri: '',
       contributors: '',
-      data: {}
+      data: {},
+      name: '',
+      url: '',
+      description: '',
+      forks: '',
+      owner: '',
+      stargazers: '',
+      issues: ''
     };
   }
 
   handleNameChange(e) {
-    this.state.data.name = e.target.value;
+    this.setState({name: e.target.value});
   }
 
   handleUrlChange(e) {
-    this.state.data.url = e.target.value;
+    this.setState({url: e.target.value});
   }
 
   handleDescriptionChange(e) {
-    this.state.data.description = e.target.value;
+    this.setState({description: e.target.value});
   }
 
   handleForksChange(e) {
-    this.state.data.forks = e.target.value;
+    this.setState({forks: e.target.value});
   }
 
   handleIssuessChange(e) {
-    this.state.data.issues = e.target.value;
+    this.setState({issues: e.target.value});
   }
 
   handleStarsChange(e) {
-    this.state.data.stargazers = e.target.value;
+    this.setState({stargazers: e.target.value});
   }
 
   handleOwnerChange(e) {
-    this.state.data.owner = e.target.value;
+    this.setState({owner: e.target.value});
   }
 
   handleContributorsChange(e) {
@@ -73,7 +78,7 @@ class Form extends Component {
       description,
     })
 
-    this.setState({data: {}, contributors: '', uri: null, repo: null, name: null});
+    this.setState({data: {}, description: '', owner: '', stargazers: '', forks: '', issues: '', contributors: '', uri: '', url: '', name: ''});
   }
 
   setUrl(e) {
@@ -83,18 +88,31 @@ class Form extends Component {
   fetchRepoData() {
     const url = this.state.url.split("/")
     api.fetchRepositoryData(url[3], url[4]).then(
-      (response) => this.setState({data: response.data.data.repositoryOwner.repository})
+      (response) => {
+        const data = response.data.data.repositoryOwner.repository;
+        const {name, url, description, forks, owner, stargazers, issues} = data;
+        this.setState({
+          data: data,
+          name: name,
+          url: url,
+          description: description,
+          forks: forks.totalCount,
+          owner: owner.login,
+          stargazers: stargazers.totalCount,
+          issues: issues.totalCount
+        })
+      }
     );
   }
 
   render() {
-    const {name, url, description, forks, owner, stargazers, issues} = this.state.data
+    const {contributors, name, url, description, forks, owner, stargazers, issues} = this.state
     return (
       <div className="Form grid">
         <h2 className="title">Enter a GitHub URL</h2>
         <div className="">
           <div name="">
-            <input className="urlForm" type="url" onChange={this.setUrl} value={this.state.url} placeholder="https://github.com/netlify/netlify-cms"/>
+            <input className="urlForm" type="url" onChange={this.setUrl} value={url} placeholder="https://github.com/netlify/netlify-cms"/>
             <p>
               <button className="button-ui-default" onClick={this.fetchRepoData}>Scrape</button>
             </p>
@@ -120,23 +138,23 @@ class Form extends Component {
             </p>
             <p>
               <label>Owner: </label>
-              <input className="boxed-input light-shadow" onChange={this.handleOwnerChange} value={owner && owner.login} name="repoowner" required />
+              <input className="boxed-input light-shadow" onChange={this.handleOwnerChange} value={owner} name="repoowner" required />
             </p>
             <p>
               <label>Contributors: </label>
-              <input className="boxed-input light-shadow" onChange={this.handleContributorsChange} value={this.state.contributors} name="contributors" />
+              <input className="boxed-input light-shadow" onChange={this.handleContributorsChange} value={contributors} name="contributors" />
             </p>
             <p>
               <label>Stars: </label>
-              <input className="boxed-input light-shadow" onChange={this.handleStarsChange} value={stargazers && stargazers.totalCount} type="text" name="stars" required />
+              <input className="boxed-input light-shadow" onChange={this.handleStarsChange} value={stargazers} type="text" name="stars" required />
             </p>
             <p>
               <label>Forks: </label>
-              <input className="boxed-input light-shadow" onChange={this.handleForksChange} value={forks && forks.totalCount} type="text" name="forks" required />
+              <input className="boxed-input light-shadow" onChange={this.handleForksChange} value={forks} type="text" name="forks" required />
             </p>
             <p>
               <label>Issues: </label>
-              <input className="boxed-input light-shadow" onChange={this.handleIssuesChange} value={issues && issues.totalCount} type="text" name="issues" required />
+              <input className="boxed-input light-shadow" onChange={this.handleIssuesChange} value={issues} type="text" name="issues" required />
             </p>
             <p>
               <textarea className="boxed-input text-box light-shadow" onChange={this.handleDescriptionChange} value={description} type="text" placeholder="Note about this repo" name="notes"></textarea>
