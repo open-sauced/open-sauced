425cbf3fa31fa193ac500486db945d776761f740
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2016-11-03 08:18:45 -0700

    adds griddle

--- src/Manager.js
+++ src/Manager.js
@@ -1,14 +1,12 @@
 import React from "react";
+import Griddle from "griddle-react";
 
 const Manager = ({data}) => {
-  return (
-    <div>
-      <ul>{data.map((repo) => (
-        <li>{repo}</li>
-      ))}</ul>
-    </div>
-  );
-}
+  const mappedRepos = data.map((x) => x.toObject());
+
+  // return <Griddle results={mappedRepos} />
+  return <Griddle results={mappedRepos} tableClassName="table" showFilter={true}
+ showSettings={true} />}
 
 export default Manager;
 
