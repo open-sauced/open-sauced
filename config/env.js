32cc50c9b75f96c6db791ad373cc20dd47fd689f
Brian Douglas <bdougie@users.noreply.github.com>
2017-01-19 18:24:15 -0800

    working env var setup (#7)

--- config/env.js
+++ config/env.js
@@ -20,7 +20,8 @@ function getClientEnvironment(publicUrl) {
       // For example, <img src={process.env.PUBLIC_URL + '/img/logo.png'} />.
       // This should only be used as an escape hatch. Normally you would put
       // images into the `src` and `import` them in code to get their paths.
-      'process.env.PUBLIC_URL': JSON.stringify(publicUrl)
+      'process.env.PUBLIC_URL': JSON.stringify(publicUrl),
+      'process.env.githubToken': JSON.stringify(process.env.GITHUB_TOKEN)
     });
 }
 
