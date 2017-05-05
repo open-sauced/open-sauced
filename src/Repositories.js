115ccc42edfc037e8c0aec6d427df895bfe29e77
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-05-05 14:28:00 -0700

    move function out of return

--- src/Repositories.js
+++ src/Repositories.js
@@ -10,26 +10,26 @@ import {
 
 const Repositories = ({match, data}) => {
   const {allRepositories} = data;
+  const content = () => (
+    <div className="landing-nav">
+      <h1>Open Sauced GitHub Reposoitories</h1>
+      <br />
+      <Instructions allRepositories={allRepositories} />
+      <ul>
+        {allRepositories ? allRepositories.map((repo) => (
+          <li key={repo.name}>
+            <Link to={`/repos/${repo.name}/${repo.id}`}>
+              {repo.name}
+            </Link>
+          </li>
+          )) : <p className="greyed">Loading...</p>}
+      </ul>
+    </div>
+  );
 
   return (
     <div className="repositories">
-      <Route exact path={match.url} render={() => (
-        <div className="landing-nav">
-          <h1>Open Sauced GitHub Reposoitories</h1>
-          <br />
-          <Instructions allRepositories={allRepositories} />
-          <ul>
-            {allRepositories ? allRepositories.map((repo) => (
-              <li key={repo.name}>
-                <Link to={`/repos/${repo.name}/${repo.id}`}>
-                  {repo.name}
-                </Link>
-              </li>
-              )) : <p className="greyed">Loading...</p>}
-          </ul>
-        </div>
-      )}
-      />
+      <Route exact path={match.url} render={content} />
       <Route path={"/repos/:repoName/:id"} component={Repository}/>
     </div>
   );
