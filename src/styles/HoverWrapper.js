5a5c616b6acdf1c49e1469e7ccd2cb0bfa4e1adb
Alex Rolfe <rolfepercussion@gmail.com>
2017-05-20 09:41:13 -0500

    Adds styled components library, new folder for styled components, and Button with Hover styling

---
+++ src/styles/HoverWrapper.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const HoverWrapper = styled.div`
+    &:hover {
+        > .button_hover {
+            background: #03b1a3;
+        }
+    }
+`;
+
+export default HoverWrapper;
