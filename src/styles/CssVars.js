2bdbdedebc132d1a9839f841a3391716e680bc30
Alex J Rolfe <arolfe@nationaldecisionsupport.com>
2017-06-10 18:33:40 -0500

    Convert css vars (#30)
    * Pulls changes from master
    :
    
    * Adds styled components library, new folder for styled components, and Button with Hover styling
    
    * Removes HoverWrapper
    
    * Replaces JSX button with Styled component button, including the background hover. Includes props adaption for 'destructive' prop on buttons for red coloring/hover
    
    * Replaces JSX button with Styled component button, including the background hover. Includes props adaption for 'destructive' prop on buttons for red coloring/hover
    
    * Removes left-over class names from components
    
    * Adds the .toml file back to the project
    
    * Adds CssVars file to hold variables for styling constants. Adds these to Button styled component attributes as proof of concept

---
+++ src/styles/CssVars.js
@@ -0,0 +1,48 @@
+const colors = {
+  offWhite: "#fbfbfb",
+  lightestGrey: "#e6e6e6",
+  lighterGrey: "#bcbcbc",
+  lightGrey: "#8b8b8b",
+  grey: "#313d3e",
+  gold: "#f6bc00",
+  orange: "#fe9d68",
+  blue: "#4d9abf",
+  green: "#00c7b7",
+  darkGreen: "#03b1a3",
+  red: "#fb6d77",
+  darkRed: "#fa3946"
+};
+
+const typography = {
+  thin: "100",
+  light: "300",
+  regular: "400",
+  semibold: "500",
+  bold: "700",
+  black: "900",
+};
+
+const padding = {
+  micro:" 8px",
+  tiny: "16px",
+  small: "24px",
+  medium: "40px",
+  large: "64px",
+  xl: "104px",
+  xxl: "168px",
+};
+
+const borderRadius = {
+  smallBorderRadius: "4px",
+  borderRadius: "10px",
+};
+
+const responsiveBreakpoints = {
+  mobile: "480px",
+  tablet: "768px",
+  blogNavBreak: "930px",
+  desktop: "960px",
+  display: "1200px"
+};
+
+export {colors, typography, padding, borderRadius, responsiveBreakpoints};
