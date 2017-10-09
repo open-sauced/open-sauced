10b5785718ff9a4d99a87d48dd4226d670a89648
Brian Douglas <bdougie@users.noreply.github.com>
2017-10-09 16:41:25 -0500

    adds service worker (#48)
    * adds service worker
    
    * updates yarn cache
    
    * adds not to readme about service worker
    
    * adds dotenv to prevent production failure

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -3,6 +3,8 @@ var webpack = require("webpack");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var InterpolateHtmlPlugin = require("react-dev-utils/InterpolateHtmlPlugin");
+const ManifestPlugin = require("webpack-manifest-plugin");
+const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 var url = require("url");
 var paths = require("./paths");
 var getClientEnvironment = require("./env");
@@ -215,6 +217,42 @@ module.exports = {
     }),
     // Note: this won"t work without ExtractTextPlugin.extract(..) in `loaders`.
     new ExtractTextPlugin("static/css/[name].[contenthash:8].css"),
+    // Generate a manifest file which contains a mapping of all asset filenames
+    // to their corresponding output file so that tools can pick it up without
+    // having to parse `index.html`.
+    new ManifestPlugin({
+      fileName: "asset-manifest.json",
+    }),
+    // Generate a service worker script that will precache, and keep up to date,
+    // the HTML & assets that are part of the Webpack build.
+    new SWPrecacheWebpackPlugin({
+      // By default, a cache-busting query parameter is appended to requests
+      // used to populate the caches, to ensure the responses are fresh.
+      // If a URL is already hashed by Webpack, then there is no concern
+      // about it being stale, and the cache-busting can be skipped.
+      dontCacheBustUrlsMatching: /\.\w{8}\./,
+      filename: "service-worker.js",
+      logger(message) {
+        if (message.indexOf("Total precache size is") === 0) {
+          // This message occurs for every build and is a bit too noisy.
+          return;
+        }
+        if (message.indexOf("Skipping static resource") === 0) {
+          // This message obscures real errors so we ignore it.
+          // https://github.com/facebookincubator/create-react-app/issues/2612
+          return;
+        }
+        console.log(message);
+      },
+      minify: true,
+      // For unknown URLs, fallback to the index page
+      navigateFallback: publicUrl + "/index.html",
+      // Ignores URLs starting from /__ (useful for Firebase):
+      // https://github.com/facebookincubator/create-react-app/issues/2237#issuecomment-302693219
+      navigateFallbackWhitelist: [/^(?!\/__).*/],
+      // Don't precache sourcemaps (they're large) and build asset manifest:
+      staticFileGlobsIgnorePatterns: [/\.map$/, /asset-manifest\.json$/],
+    }),
   ],
   // Some libraries import Node modules but don"t use them in the browser.
   // Tell Webpack to provide empty mocks for them so importing them works.
---
+++ src/service-worker.js
@@ -0,0 +1,38 @@
+const cacheName = "OpenSaucedPWA-v1";
+const filesToCache = ["index.html"];
+
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  console.log("[ServiceWorker] Install");
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log("[ServiceWorker] Caching app shell");
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("activate", function(event) {
+  console.log("[Servicework] Activate");
+  event.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log("[ServiceWorker] Removing old cache shell", key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  // event.respondWith(fetchAndReplace(event.request));
+  console.log("[ServiceWorker] Fetch");
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+
+});
