208fc6090933017c20120bf0926262aadff9e652
Brian Douglas <bdougie@users.noreply.github.com>
2017-10-14 01:29:39 -0700

    more query org (#57)
    * more query org
    
    * updates yarn lock

--- src/queries/repoQueries.js
+++ src/queries/repoQueries.js
@@ -1,6 +1,6 @@
 import gql from "graphql-tag";
 
-const allRepoQuery = gql`
+export const allRepoQuery = gql`
  query {
     allRepositories {
       id
@@ -9,7 +9,21 @@ const allRepoQuery = gql`
   }
 `;
 
-const createRepo = gql`
+export const repoQuery = gql`query RepositoryQuery($id: ID!) {
+  Repository(id: $id) {
+    id
+    name
+    owner
+    url
+    issuesCount: issues
+    forksCount: forks
+    stars: stargazers
+    notes
+    description
+  }
+}`;
+
+export const createRepo = gql`
   mutation updateRepository($notes: String, $id: ID!) {
     updateRepository(id: $id, notes: $notes) {
       id
@@ -18,12 +32,10 @@ const createRepo = gql`
   }
 `;
 
-const deleteRepo = gql`
+export const deleteRepo = gql`
   mutation deleteRepo($id: ID!) {
     deleteRepository(id: $id) {
       id
     }
   }
 `;
-
-export default {allRepoQuery, createRepo, deleteRepo};
