c1567105f238967180472e3efd0d0c190fa81e7a
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2017-12-01 06:04:46 -0800

    upgrade test suite an ddepencies for react 16

--- src/tests/Repositories.test.js
+++ src/tests/Repositories.test.js
@@ -1,11 +1,19 @@
 import React from "react";
 import {shallow} from "enzyme";
 import {Repositories} from "../containers/Repositories";
-import {data, match} from "./mocks";
+
+import {LocalStorageMock, data, match} from "./mocks";
+import {promisify} from 'util';
+
+// set up local storage mock usage in component
+global.localStorage = new LocalStorageMock
+localStorage.setItem("currentOpenSaucedUser", JSON.stringify({...data}));
+
+const emptyPromise = promisify(() => {})
 
 describe("<Repositories />", () => {
   it("should render without throwing an error", () => {
-    const component = shallow(<Repositories match={match} data={data} />);
+    const component = shallow(<Repositories createViewer={emptyPromise} match={match} data={data} />);
     expect(component).toMatchSnapshot();
   });
 });
