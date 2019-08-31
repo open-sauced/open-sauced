c5a9abea5536c68f6ad2671d375430e5f8c90623
Sean Grove <sean@bushi.do>
2019-08-30 19:34:09 -0700

    Convert to using the OneGraph code-export

--- src/config.js
+++ src/config.js
@@ -6,4 +6,24 @@ const auth = new OneGraphAuth({
   appId: APP_ID
 });
 
-export default {auth: auth, appId: APP_ID};
+// This setup is only needed once per application
+const fetchOneGraph = async(operationsDoc, operationName, variables) => {
+  const result = await fetch(
+    "https://serve.onegraph.com/dynamic?app_id=" + APP_ID,
+    {
+      method: "POST",
+      headers: {
+        ...auth.authHeaders()
+      },
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName
+      })
+    }
+  );
+
+  return await result.json();
+};
+
+export default {auth: auth, appId: APP_ID, fetchOneGraph: fetchOneGraph};
