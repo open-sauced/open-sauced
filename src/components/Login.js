288d40ff4d1baae5a75f5fcce31d971247383d17
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-09-01 06:25:47 -0700

    copy edits

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import {createViewer} from "../queries";
 import {graphql} from "react-apollo";
 import logo from "./logo8.svg";
 import {FlexCenter} from "../styles/Grid";
+import Button from "../styles/Button";
 
 const spacingStyle = {margin: "auto", padding: 100, textAlign: "center"};
 
@@ -13,8 +14,8 @@ export class Login extends Component {
       <FlexCenter>
         <div style={spacingStyle}>
           <img src={logo} alt="logo" />
-          <p style={{marginTop: 16}}>The path towards open-source contributions. Login to get Saucin'</p>
-          <button onClick={handleLogIn}>Sign in with GitHub</button>
+          <p style={{marginTop: 16}}>The path towards open-source contributions.</p>
+          <Button onClick={handleLogIn}>Login to get Saucin</Button>
         </div>
       </FlexCenter>
     );
