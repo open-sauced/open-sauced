737a30a075828aad1e77e55e649bfaf6db1669d9
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-09-13 16:42:31 -0700

    swap out mention in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import PrimaryLink from "../styles/Typography/PrimaryLink";
 const Footer = () => (
   <footer>
     <small>
-      Made with <PrimaryLink href="https://www.prisma.io/">Prisma</PrimaryLink> using{" "}
+      Made with <PrimaryLink href="https://www.onegraph.com/">OneGraph</PrimaryLink> using{" "}
       <PrimaryLink href="https://feathericons.com/">Feather</PrimaryLink> icons and hosted on{" "}
       <PrimaryLink href="https://www.netlify.com/">Netlify</PrimaryLink>.
     </small>
