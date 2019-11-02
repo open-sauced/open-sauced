2242dd18ad17972eb9b4f55559afb97e725cc723
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-11-02 11:58:15 -0700

    fix the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {home, github, plus, logout, issue} from "../icons";
 import {Link} from "react-router-dom";
-import {FloatLeft} from "../styles/Grid";
+import {FloatLeft, FloatRight} from "../styles/Grid";
 import {SimpleHeader} from "../styles/Header";
 
 function Header({user, handleLogOut}) {
@@ -18,24 +18,26 @@ function Header({user, handleLogOut}) {
         </Link>
       </FloatLeft>
 
-      <a className="logout" onClick={handleLogOut}>
-        <span>
-          <img src={logout} />
-        </span>
-      </a>
-      <a className="nav-link" target="_blank" href="https://github.com/bdougie/open-sauced/issues/new">
-        <span>
-          <img src={issue} />
-        </span>
-      </a>
-      <a className="nav-link" href="https://github.com/bdougie/open-sauced">
-        <span>
-          <img src={github} />
-        </span>
-      </a>
-      <a className="nav-link" href={`https://github.com/${user.login}`}>
-        <span>Hi, {user.login}!</span>
-      </a>
+      <FloatRight>
+        <a className="nav-link" href={`https://github.com/${user.login}`}>
+          <span>Hi, {user.login}!</span>
+        </a>
+        <a className="nav-link" target="_blank" href="https://github.com/bdougie/open-sauced/issues/new">
+          <span>
+            <img src={issue} />
+          </span>
+        </a>
+        <a className="nav-link" href="https://github.com/bdougie/open-sauced">
+          <span>
+            <img src={github} />
+          </span>
+        </a>
+        <a className="logout" onClick={handleLogOut}>
+          <span>
+            <img src={logout} />
+          </span>
+        </a>
+      </FloatRight>
     </SimpleHeader>
   );
 }
--- src/styles/Grid/index.js
+++ src/styles/Grid/index.js
@@ -1,6 +1,6 @@
 import {FlexCenter, FlexHeader, SpaceBetween} from "./Flex";
 import IssuesColumn from "./IssuesColumn";
 import FormColumn from "./FormColumn";
-import FloatLeft from "./FloatLeft";
+import {FloatRight, FloatLeft} from "./Float";
 
-export {SpaceBetween, FlexCenter, FlexHeader, IssuesColumn, FormColumn, FloatLeft};
+export {SpaceBetween, FlexCenter, FlexHeader, IssuesColumn, FormColumn, FloatLeft, FloatRight};
