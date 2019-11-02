5148ccfc377e5082913274686955ab232d21e8d6
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-11-02 07:34:00 -0700

    remove unused variables

--- src/styles/Gradient.js
+++ src/styles/Gradient.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {borderRadius, colors, size} from "./variables";
+import {colors} from "./variables";
 
 export const Container = styled.div`
   background-image: linear-gradient(${colors.backgroundGrey}, ${colors.offWhite} 900px);
