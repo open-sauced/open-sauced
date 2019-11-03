baac7ed6a38d9231d2295ed0c909ca4a732b7593
brian <ilikerobot@gmail.com>
2019-11-03 08:44:59 -0800

    fix mobile view

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,19 +1,17 @@
 import React from "react";
 import logo from "./logo8.svg";
-import {FlexCenter} from "../styles/Grid";
+import {FlexColumnCenter} from "../styles/Grid";
 import Button from "../styles/Button";
-
-const spacingStyle = {margin: "auto", padding: 100, textAlign: "center"};
 
 function Login({handleLogIn}) {
   return (
-    <FlexCenter>
-      <div style={spacingStyle}>
-        <img src={logo} alt="logo" />
-        <p style={{marginTop: 16}}>The path towards open-source contributions.</p>
-        <Button primary onClick={handleLogIn}>Login with GitHub</Button>
-      </div>
-    </FlexCenter>
+    <FlexColumnCenter>
+      <img src={logo} alt="logo" />
+      <p style={{marginTop: 16}}>The path towards open-source contributions.</p>
+      <Button primary onClick={handleLogIn}>
+        Login with GitHub
+      </Button>
+    </FlexColumnCenter>
   );
 }
 
--- src/styles/Label.js
+++ src/styles/Label.js
@@ -1,11 +1,17 @@
 import styled from "styled-components";
 import {borderRadius, colors} from "./variables";
+import MEDIA from "./mediaTemplates";
 
 const Label = styled.p`
   border: ${colors.lighterGrey} solid 2px;
   padding: 3px;
   border-radius: ${borderRadius};
   margin-right: 24px !important;
+
+  ${MEDIA.TABLET`
+    display: none;
+  `};
+
 `;
 
 export default Label;
