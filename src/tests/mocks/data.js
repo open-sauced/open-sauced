4ec21af5d1e39f2c74c06dbdd47acf8b342a7988
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-11-05 17:14:40 -0800

    adds Header component tests

--- src/tests/mocks/data.js
+++ src/tests/mocks/data.js
@@ -1,13 +1,13 @@
 const data = {
+  user: {login: "sam"},
   allRepositories: [
     {
       id: "123",
       name: "Platanito",
       email: "email@mail.com",
-    }
-
+    },
   ],
-  refetch: () => {}
+  refetch: () => {},
 };
 
 export default data;
