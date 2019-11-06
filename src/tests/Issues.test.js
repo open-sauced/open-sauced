222b3cc5a1e53dc093a620d997ceadb827c805fb
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-11-06 06:37:12 -0800

    update issues test

--- src/tests/Issues.test.js
+++ src/tests/Issues.test.js
@@ -1,11 +1,21 @@
 import React from "react";
-import {shallow} from "enzyme";
 import Issues from "../containers/Issues";
+import "@testing-library/jest-dom/extend-expect";
+import {render, cleanup} from "@testing-library/react";
+import {axe, toHaveNoViolations} from "jest-axe";
+expect.extend(toHaveNoViolations);
 
-describe("<Issues />", () => {
-  it("should render without throwing an error", () => {
-    const component = shallow(<Issues />);
-    expect(component).toBeDefined();
-    expect(component.exists()).toBe(true);
-  });
+import {data} from "./mocks";
+
+test("container component should have no violations", async () => {
+  const {container} = render(<Issues owner={data.user} />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+
+  cleanup();
+});
+
+test("renders loading when there is no owner", () => {
+  const {container} = render(<Issues />);
+  expect(container.firstChild).toHaveTextContent("...Loading");
 });
