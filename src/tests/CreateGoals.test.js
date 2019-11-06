746b39eb3348a3fbd518c96e5af0364cd7c15358
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-11-06 06:18:01 -0800

    adds creategoals test

---
+++ src/tests/CreateGoals.test.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import {render, cleanup} from "@testing-library/react";
+import CreateGoals from "../components/CreateGoals";
+import {data} from "./mocks";
+import {axe, toHaveNoViolations} from "jest-axe";
+expect.extend(toHaveNoViolations);
+
+const {goals} = data;
+
+test("container component should have no violations", async () => {
+  const {container} = render(<CreateGoals />);
+  const results = await axe(container);
+  expect(results).toHaveNoViolations();
+
+  cleanup();
+});
+
+test("should render a link to track first repository if there are no repositories", () => {
+  const mockData = [];
+  const {container} = render(<CreateGoals />);
+  expect(container).toHaveTextContent("To get saucin' create a goals");
+});
+
