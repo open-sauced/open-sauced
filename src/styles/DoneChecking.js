94184ce70ccfa6a24a600ce028609298ac34bce9
ckopecky <kopecky12112@gmail.com>
2019-11-07 15:19:30 -0800

    Use style component for vector graphic

---
+++ src/styles/DoneChecking.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import {borderRadius} from "./variables";
+
+const DoneChecking = styled.img`
+  width: 30%;
+  -moz-border-radius: 5px;
+  -webkit-border-radius: ${borderRadius};
+  border-radius: 5px;
+  padding: 0 20px
+`;
+
+export default DoneChecking;
