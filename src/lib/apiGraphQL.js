c7cecc6149dfbe30371d9e2abcfb0c7151f378b7
Nilesh Patel <nilesh.nvs@hotmail.com>
2019-11-11 00:21:29 +0530

    refactor-code

--- src/lib/apiGraphQL.js
+++ src/lib/apiGraphQL.js
@@ -290,7 +290,7 @@ function createOpenSaucedGoalsRepo() {
 }
 
 function createGoal(repoId, title, notes) {
-  return fetchOneGraph(operationsDoc, "CreateGoal", {repoId: repoId, title: title, body: notes});
+  return fetchOneGraph(operationsDoc, "CreateGoal", {repoId: repoId, title: title, notes: notes});
 }
 
 function updateGoal(id, title, state, notes) {
