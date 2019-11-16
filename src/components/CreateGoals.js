9356fc40edb6cafc6a8fc9803a364f6145043767
brian <ilikerobot@gmail.com>
2019-11-16 08:20:56 -0800

    working repo creation refresh

--- src/components/CreateGoals.js
+++ src/components/CreateGoals.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Button from "../styles/Button";
+import api from "../lib/apiGraphQL";
 
-function CreateGoals({handleGoalCreation}) {
+function CreateGoals({onRepoCreation}) {
+  const _handleRepoCreation = () => {
+    api.createOpenSaucedGoalsRepo().then(res => {
+      onRepoCreation(res.data.gitHub.createRepository.repository);
+    });
+  };
+
   return (
     <div>
       <p>To get saucin' create a goals</p>
-      <Button onClick={handleGoalCreation}>Create your goal workspace</Button>
+      <Button onClick={_handleRepoCreation}>Create your goal workspace</Button>
     </div>
   );
 }
