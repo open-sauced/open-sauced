d0d960b63a527de04118431a260f52fff6e4673d
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-04-17 19:51:47 -0700

    adjust image sizes

--- stories/6-Login.stories.js
+++ stories/6-Login.stories.js
@@ -6,23 +6,24 @@ export default {
 };
 
 export const Hero = () => (
-  <section style={{width: 1920}}>
-    <div style={{height: 900}}>
-      <img src={sauce} />
-      <img src={pizza} />
-    </div>
-    <div style={{backgroundColor: "#fff"}} className="navbar color-white">
-      menu
-    </div>
-    <div style={{height: 250, backgroundColor: "#faeace"}} className="slogan1 color-accent">
-      slogan1{" "}
-    </div>
-    <div style={{height: 140, backgroundColor: "#f6d809"}} className="slogan2 login color-yellow">
-      slogan2{" "}
-    </div>
-    <div style={{height: 50}} className="trusting-bar color-white">
-      trust us
+  <section style={{width: 500}}>
+    <div className="Hero Component">
+      <div style={{backgroundColor: "#fff"}} className="navbar color-white">
+        menu
+      </div>
+      <div style={{height: 120, backgroundColor: "#faeace"}} className="slogan1 color-accent">
+        slogan1{" "}
+        <div style={{float: "right"}} className="Side-Image">
+          <img style={{height: 220, position: "relative", top: 0, right: -146}} src={sauce} />
+          <img style={{height: 165, position: "relative", top: -36, right: -42}} src={pizza} />
+        </div>
+      </div>
+      <div style={{height: 70, backgroundColor: "#f6d809"}} className="slogan2 login color-yellow">
+        slogan2{" "}
+      </div>
+      <div style={{height: 50}} className="trusting-bar color-white">
+        trust us
+      </div>
     </div>
   </section>
 );
-
