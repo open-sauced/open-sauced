70b77ea46b0953d74dbf9f12fed72efdb6714ae3
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-04-19 06:15:49 -0700

    menu bar good enough

--- stories/6-Login.stories.js
+++ stories/6-Login.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {sauce, pizza} from "../src/icons";
+import {logo1 as logo} from "../src/logos";
 
 export default {
   title: "LoginPage",
@@ -8,8 +9,14 @@ export default {
 export const Hero = () => (
   <section style={{width: 500}}>
     <div className="Hero Component">
-      <div style={{backgroundColor: "#fff"}} className="navbar color-white">
-        menu
+      <div style={{display: "flex", alignItems: "baseline", justifyContent: "space-between", fontSize: 8, backgroundColor: "#fff"}} className="navbar color-white">
+        <ul style={{padding: 0, listStyle: "none", display: "inline-flex"}}>
+          <li style={{textTransform: "uppercase", marginRight: 4}}>Blog</li>
+          <li style={{textTransform: "uppercase", marginRight: 4}}>GitHub</li>
+          <li style={{textTransform: "uppercase", marginRight: 4}}>Subscribe</li>
+          <li style={{textTransform: "uppercase", marginRight: 4}}>Login</li>
+        </ul>
+        <img style={{height: 8}} src={logo} />
       </div>
       <div style={{height: 120, backgroundColor: "#faeace"}} className="slogan1 color-accent">
         slogan1{" "}
