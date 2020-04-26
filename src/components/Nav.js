f67f4b03196a7ff52f6152a02d95122871d7c44d
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-04-26 09:55:45 -0700

    organize nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,48 +6,61 @@ import {AppNav, HomePageHeader as HomeNav} from "../styles/Header";
 import {SpaceBetween} from "../styles/Grid";
 import {Link} from "react-router-dom";
 
+function LeftSide({user, handleLogIn, handleLogOut}) {
+  return (
+    <FloatLeft>
+      <ul>
+        <li>
+          <SubtleLink href="https://dev.to/t/opensauced">Blog</SubtleLink>
+        </li>
+        <li>
+          <SubtleLink href="https://github.com/bdougie/open-sauced">GitHub</SubtleLink>
+        </li>
+        <li>
+          <SubtleLink href="#">Subscribe</SubtleLink>
+        </li>
+        {user && (
+          <li>
+            <SubtleLink className="nav-link" target="_blank" href="https://github.com/bdougie/open-sauced/issues/new">
+              Issue
+            </SubtleLink>
+          </li>
+        )}
+        <li>
+          {user ? (
+            <SubtleLink onClick={handleLogOut}>Logout</SubtleLink>
+          ) : (
+            <SubtleLink onClick={handleLogIn}>Login</SubtleLink>
+          )}
+        </li>
+      </ul>
+    </FloatLeft>
+  );
+}
+
+function RightSide({user}) {
+  return (
+    <FloatRight>
+      <SpaceBetween>
+        {user && (
+          <SubtleLink alt="user login name" className="nav-link" href={`https://github.com/${user.login}`}>
+            <span title="login name">Hi, {user.login}!</span>
+          </SubtleLink>
+        )}
+        <Link to="/">
+          <img alt="open sauced" src={logo} />
+        </Link>
+      </SpaceBetween>
+    </FloatRight>
+  );
+}
+
 function Header({user, handleLogOut, handleLogIn}) {
   const Nav = user ? AppNav : HomeNav;
   return (
     <Nav>
-      <FloatLeft>
-        <ul>
-          <li>
-            <SubtleLink href="https://dev.to/t/opensauced">Blog</SubtleLink>
-          </li>
-          <li>
-            <SubtleLink href="https://github.com/bdougie/open-sauced">GitHub</SubtleLink>
-          </li>
-          <li>
-            <SubtleLink href="#">Subscribe</SubtleLink>
-          </li>
-          {user && <li>
-            <SubtleLink className="nav-link" target="_blank" href="https://github.com/bdougie/open-sauced/issues/new">
-              Issue
-            </SubtleLink>
-          </li>}
-          <li>
-            {user ? (
-              <SubtleLink onClick={handleLogOut}>Logout</SubtleLink>
-            ) : (
-              <SubtleLink onClick={handleLogIn}>Login</SubtleLink>
-            )}
-          </li>
-        </ul>
-      </FloatLeft>
-
-      <FloatRight>
-        <SpaceBetween>
-          {user && (
-            <SubtleLink alt="user login name" className="nav-link" href={`https://github.com/${user.login}`}>
-              <span title="login name">Hi, {user.login}!</span>
-            </SubtleLink>
-          )}
-          <Link to="/">
-            <img alt="open sauced" src={logo} />
-          </Link>
-        </SpaceBetween>
-      </FloatRight>
+      <LeftSide handleLougOut={handleLogOut} handleLogIn={handleLogIn} user={user} />
+      <RightSide user={user} />
     </Nav>
   );
 }
