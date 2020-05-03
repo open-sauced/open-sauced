3c5ae9c2e1cbd8fe6ebcce60ee7d7ca80d29bfa7
bdougie <bdougie@users.noreply.github.com>
2020-05-03 06:03:51 +0000

    Deploying to storybook-static from  @ 7962c42f2d4d6b8a336ed09e824d43f265432439 🚀

---
+++ main.4b082bc991d827057be4.bundle.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{1514:function(n,o,c){"use strict";c.r(o);c(1515),c(1571),c(1574),c(1578)},520:function(n,o,c){c(521),c(810),n.exports=c(1514)},541:function(n,o){},636:function(n,o){},686:function(n,o){},725:function(n,o){},756:function(n,o){}},[[520,1,2]]]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.5ae7734b587cd6e989463f282298d7c6.js"
+  "precache-manifest.75efc9ea836548a4093cf5beced66f80.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
