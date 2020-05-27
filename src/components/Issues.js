f568d182f61677d96d35b42f0279ed09198b85fd
Yogi <me@yogi.codes>
2020-05-27 07:48:29 +0000

    Init Empty Placeholder

--- src/components/Issues.js
+++ src/components/Issues.js
@@ -52,7 +52,7 @@ function Issues({repoName, owner}) {
   const currentPage = offset / 5 + 1;
 
   return owner ? (
-    totalCount > 0 && (
+    totalCount > 0 ? (
       <Card fitted>
         <CardPadding>
           <h1>Issues</h1>
@@ -75,6 +75,8 @@ function Issues({repoName, owner}) {
           </CardPadding>
         </List>
       </Card>
+    ) : (
+      <p>Nothing Here</p>
     )
   ) : (
     <p>...Loading</p>
