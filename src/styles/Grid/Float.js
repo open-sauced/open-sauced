cf9488aeaaf578fb1bd9341aed96c8acf6ea1a40
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-05-28 21:44:33 -0700

    fix header

--- src/styles/Grid/Float.js
+++ src/styles/Grid/Float.js
@@ -1,7 +1,15 @@
 import styled from "styled-components";
+import MEDIA from "../mediaTemplates";
 
 const FloatLeft = styled.div`
   float: left;
+  ${MEDIA.TABLET`
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    align-items: center;
+  `};
+
 `;
 
 const FloatRight = styled.div`
