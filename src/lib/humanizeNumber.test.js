89a8e931ab348a5efd37eed495c351a2d49e8cd7
Brian Douglas <bdougie@users.noreply.github.com>
2020-06-02 05:00:26 -0700

    Update src/lib/humanizeNumber.test.js
    add a different test

--- src/lib/humanizeNumber.test.js
+++ src/lib/humanizeNumber.test.js
@@ -7,9 +7,9 @@ describe("Test: humanizeNumber()", () => {
     expect(result).toBe("1,000");
   });
 
-  test(`'1000' should humanize`, () => {
-    const num = "1000";
+  test(`'100' should not humanize`, () => {
+    const num = "100";
     const result = humanizeNumber(num);
-    expect(result).toBe("1,000");
+    expect(result).toBe("100");
   });
 });
