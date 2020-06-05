332db8ef8dccab261a9badde2b1ce02c3f092457
Yogi <61746440+yg@users.noreply.github.com>
2020-06-05 07:33:11 +0000

    Fix

--- src/components/ListGoals.js
+++ src/components/ListGoals.js
@@ -4,16 +4,13 @@ import {Container} from "../styles/ListItem";
 import RepoListItem from "../components/RepoListItem";
 import Card from "../components/Card";
 import List from "../styles/List";
-import {EmptyPlaceholder} from "../styles/EmptyPlaceholder";
-import Octicon, {getIconByName} from "@primer/octicons-react";
 
 function ListGoals({goals}) {
   return (
     <Container>
       <Card fitted>
         <List>
-          {goals.totalCount > 0 ? (
-            goals &&
+          {goals &&
             goals.nodes.map(goal => (
               <li key={goal.id}>
                 <Link
@@ -23,17 +20,7 @@ function ListGoals({goals}) {
                   <RepoListItem goal={goal} stars={328} contributors={18} />
                 </Link>
               </li>
-            ))
-          ) : (
-            <EmptyPlaceholder>
-              <div style={{color: "grey"}}>
-                <Octicon size="large" verticalAlign="middle" icon={getIconByName("rocket")} />
-              </div>
-              <div className="helper">
-                No Goals created
-              </div>
-            </EmptyPlaceholder>
-          )}
+            ))}
         </List>
       </Card>
     </Container>
