e9e95dd72dc8c016ea5c490a0b4cfa748b504551
Yogi <61746440+yg@users.noreply.github.com>
2020-06-06 04:08:42 +0000

    Admin bar will not show when logged out

--- src/index.js
+++ src/index.js
@@ -73,8 +73,10 @@ function Index() {
       localStorage.removeItem("oneGraph:" + Config.appId);
       // Remove the local AdminStats bar status storage
       localStorage.removeItem("adminBar");
+      // Remove the local logged in status storage
       localStorage.removeItem("isLoggedIn");
       setUser(null);
+      setIsAdmin(false);
       setLogin(false);
     });
   };
