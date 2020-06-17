8a6794fdc57661f00a8a79bbd6bec4d68a4308d3
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-17 11:53:59 +0200

    Add language difference as a const
    Co-Authored-By: Yogi <me@yogi.codes>

--- src/components/Repository.js
+++ src/components/Repository.js
@@ -61,7 +61,7 @@ function Repository({match}) {
   }, []);
 
   const {url, stargazers, forks, issues, name, nameWithOwner, owner} = repository || {};
-
+  const totalLangDiff = repository && repository.languages.totalCount - languagesShown;
   return (
     <section>
       {error && <ErrorMessage>{error}</ErrorMessage>}
@@ -94,7 +94,11 @@ function Repository({match}) {
                 </span>
               ))}
               <span className="more">
-                {repository && repository.languages.totalCount > languagesShown && `+${repository.languages.totalCount - languagesShown} language${repository.languages.totalCount - languagesShown !== 1 ? "s" : ""}`}
+                {
+                  repository &&
+                  repository.languages.totalCount > languagesShown &&
+                  `+${totalLangDiff} language${totalLangDiff !== 1 ? "s" : ""}`
+                }
               </span>
             </div>
           </div>
