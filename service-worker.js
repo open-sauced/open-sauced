1c680b6ef9649a27c6980df71c6e4cd4dc639b7c
bdougie <bdougie@users.noreply.github.com>
2020-06-18 06:01:05 +0000

    Deploying to storybook-static from  @ c3eb79751e9609ce6513cd91419185592c1a4dd7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6d934aaf953ad97f0a06477118007ffa.js"
+  "precache-manifest.47921bf4711657cae30936229e734a3d.js"
 );
 
 self.addEventListener('message', (event) => {
