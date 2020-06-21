f18e6abb67fb6363581f45fc90ceccb205d53b56
Yogi <61746440+yg@users.noreply.github.com>
2020-06-21 06:08:06 +0000

    Add repoStatusCode test

---
+++ src/lib/repoStatusCode.test.js
@@ -0,0 +1,15 @@
+import {repoStatusCode} from "./repoStatusCode";
+
+describe("Test: repoStatusCode()", () => {
+  test("github/gh-ost should return 200", async() => {
+    const repoUrl = "github/gh-ost";
+    const result = await repoStatusCode(repoUrl);
+    expect(result).toBe(200);
+  });
+
+  test("github/ghost should return 404", async() => {
+    const repoUrl = "github/ghost";
+    const result = await repoStatusCode(repoUrl);
+    expect(result).toBe(404);
+  });
+});
