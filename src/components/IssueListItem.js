db9b8be8f11489330186f34f718c6f0c212abe91
Filip Troníček <filip.tronicek@seznam.cz>
2020-07-03 15:55:57 +0200

    Set color of text based on contrast (#763)
    * Set color of text based on contrast
    
    Co-Authored-By: Brian Douglas <ilikerobot@gmail.com>
    
    * Move the function and add optional params
    
    * Change threshold
    
    * Correct spelling of threshold
    
    * Add and use the contrast pckg instead
    
    Co-authored-by: Brian Douglas <ilikerobot@gmail.com>

--- src/components/IssueListItem.js
+++ src/components/IssueListItem.js
@@ -4,6 +4,7 @@ import {chevronRight} from "../icons";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 import {IssueOpenedIcon, CommentIcon, MilestoneIcon} from "@primer/octicons-react";
+import contrast from "contrast";
 
 dayjs.extend(relativeTime);
 
@@ -24,7 +25,7 @@ function IssueListItem({title, labels, author, opened, type, participants, comme
             <div>
               {labels.data.length > 0 && labels.data.map(label => (
                 <span
-                  style={{backgroundColor: `#${label.node.color}`}}
+                  style={{backgroundColor: `#${label.node.color}`, color: `${contrast(label.node.color) === "light" ? "black" : "white"}`}}
                   key={label.node.id}>
                   {label.node.name}
                 </span>
