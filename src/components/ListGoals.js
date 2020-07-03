c1d5fe6ed3daa6c9e491fb436d615ac4c80dfdac
Yogi <61746440+yg@users.noreply.github.com>
2020-07-03 11:16:26 +0530

    Add Basic sort

--- src/components/ListGoals.js
+++ src/components/ListGoals.js
@@ -1,20 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from "react-router-dom";
 import {Container} from "../styles/ListItem";
 import RepoListItem from "../components/RepoListItem";
 import Card from "../components/Card";
 import List from "../styles/List";
 import {merge} from "lodash";
+import sortBy from "lodash/sortBy";
 
 function ListGoals({goals, data}) {
+  const [sortType, setSortType] = useState("full_name");
   const goalsWithData = merge(goals.nodes, data);
+
+  console.log(goalsWithData);
 
   return (
     <Container>
+      <select value={sortType} onChange={e => setSortType(e.currentTarget.value)}>
+        <option value="wip">None</option>
+        <option value="full_name">Name</option>
+        <option value="stargazers_count">Most Stars</option>
+        <option value="stargazers_count">Fewest Stars</option>
+      </select>
       <Card fitted>
         <List>
           {goalsWithData &&
-            goalsWithData.map(goal => (
+            sortBy(goalsWithData, sortType).map(goal => (
               <li key={goal.id}>
                 <Link
                   to={{
