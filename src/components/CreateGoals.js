d1438b8e3e6633750cac560834c97d07279e3579
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2020-07-04 23:00:43 -0700

    adds install instructions

--- src/components/CreateGoals.js
+++ src/components/CreateGoals.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Button from "../styles/Button";
 import Illustration from "../styles/Illustration";
 import {ContextStyle} from "../styles/Card";
@@ -7,7 +7,41 @@ import api from "../lib/apiGraphQL";
 import {goalsReducer} from "../lib/reducers";
 import {devProductive} from "../illustrations";
 
+function CreateApp() {
+  return (
+    <React.Fragment>
+      <h1>Create the Goals Repository</h1>
+      <p>
+        Open Sauced is a tool to help track your next open source contributions. You can get started by creating a goal
+        workspace below.
+      </p>
+      <p>A public repository name "open-sauced-goals" will be created on your GitHub account to store.</p>
+      <small>
+        <em>You own all your data saved while saucin.</em>
+      </small>
+    </React.Fragment>
+  );
+}
+
+function InstallApp() {
+  return (
+    <React.Fragment>
+      <h1>Install the GitHub App</h1>
+      <p>
+        GitHub Apps are the officially recommended way to integrate with GitHub because they offer much more granular
+        permissions to access data.
+      </p>
+      <p>The Open Sauced App needs to be installed on your newly created "open-sauced-goals" public repository.</p>
+      <img style={{textAlign: "center", width: "80%"}} src="https://user-images.githubusercontent.com/20134767/86527180-4a83c700-be51-11ea-8eaf-660298cf3c66.png" />
+      <small>
+        <em>The installation grants access storing note data and tracking open source contributions.</em>
+      </small>
+    </React.Fragment>
+  );
+}
+
 function CreateGoals({installNeeded, user, onRepoCreation}) {
+  const [installReady, setInstallReady] = useState(installNeeded);
   const _handleRepoCreation = () => {
     api.fetchOwnerId(user.login).then(ownerRes => {
       const {
@@ -18,22 +52,30 @@ function CreateGoals({installNeeded, user, onRepoCreation}) {
         },
       } = ownerRes;
 
-      api.createOpenSaucedGoalsRepo(id).then(goalsRes => {
-        const {errors, data} = goalsRes;
+      api
+        .createOpenSaucedGoalsRepo(id)
+        .then(goalsRes => {
+          const {errors, data} = goalsRes;
 
-        if (errors && errors.length > 0) {
-          console.log(`"${errors[0].message}"`);
-        }
-        const {
-          gitHub: {
-            cloneTemplateRepository: {
-              repository: {id},
-            },
-          },
-        } = data;
+          if (errors && errors.length > 0) {
+            console.log(`"${errors[0].message}"`);
+          }
 
-        onRepoCreation(id, goalsReducer(goalsRes, {type: "CREATE"}));
-      });
+          setInstallReady(data.gitHub === null);
+
+          if (data.gitHub) {
+            const {
+              gitHub: {
+                cloneTemplateRepository: {
+                  repository: {id},
+                },
+              },
+            } = data;
+
+            onRepoCreation(id, goalsReducer(goalsRes, {type: "CREATE"}));
+          }
+        })
+        .catch(err => console.log(err));
     });
   };
 
@@ -41,25 +83,16 @@ function CreateGoals({installNeeded, user, onRepoCreation}) {
     <React.Fragment>
       <ContextStyle>
         <SpaceBetween>
-          <FlexColumn>
-            <React.Fragment>
-              <h1>Get Saucin'</h1>
-              <p>
-                Open Sauced is a tool to help track your next open source contributions. You can get started by creating
-                a goal workspace below.
-              </p>
-              <p>A public repository name "open-sauced-goals" will be created on your GitHub account to store.</p>
-              <small>
-                <em>You own all your data saved while saucin.</em>
-              </small>
-            </React.Fragment>
-          </FlexColumn>
+          <FlexColumn>{installReady ? <InstallApp /> : <CreateApp />}</FlexColumn>
           <Illustration alt="productive developer image" src={devProductive} />
         </SpaceBetween>
       </ContextStyle>
       <br style={{marginTop: 8}} />
-      {installNeeded ? (
-        <a referrer="noreferrer" target="_blank" href={`https://github.com/apps/open-sauced/installations/new/permissions?target_id=${user.id}`}>
+      {installReady ? (
+        <a
+          referrer="noreferrer"
+          target="_blank"
+          href={`https://github.com/apps/open-sauced/installations/new/permissions?target_id=${user.id}`}>
           <Button primary>Finish initializing {user.login}/open-sauced-goals</Button>
         </a>
       ) : (
