83e1de0698f8338e4e486e80dcf0a145a238e11f
Yogi <61746440+yg@users.noreply.github.com>
2020-07-05 05:14:54 +0000

    Reduce margin for meta and label

--- src/styles/Loader.js
+++ src/styles/Loader.js
@@ -1,11 +1,11 @@
 import styled from "styled-components";
-import {size} from "./variables";
+import {size, margin} from "./variables";
 
 const Loader = styled.div`
   padding: 0px ${size.small} 0 ${size.micro};
 
   .meta, .label {
-    margin-top: ${size.tiny}
+    margin-top: ${margin.gutter}
   }
 `;
 
