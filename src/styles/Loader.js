619bd42a7eb79af764032ab37ae88da1272ceb5d
Yogi <61746440+yg@users.noreply.github.com>
2020-07-05 04:56:20 +0000

    Fix Alignment

--- src/styles/Loader.js
+++ src/styles/Loader.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 import {size} from "./variables";
 
 const Loader = styled.div`
-  padding: 0px 16px;
+  padding: 0px ${size.small} 0 ${size.micro};
 
   .meta, .label {
     margin-top: ${size.tiny}
