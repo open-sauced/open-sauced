b54c6b5a24cdc96d91d92f9c4c105ca255d5d147
Jason Davis <jason@jasonericdavis.dev>
2020-08-20 16:09:13 -0400

    Fix Issue #807:  App crashes if OneGraph token has expired (#808)
    * App crashes if OneGraph token has expired.
    
    * moved token validation to lib and added tests
    
    * removed a stray console.log statement

---
+++ src/lib/validateToken.test.js
@@ -0,0 +1,21 @@
+import {validateToken} from './validateToken'
+
+describe("Test: validateToken()", () => {
+    test("valid token", () => {
+        const tomorrow = new Date() 
+        tomorrow.setDate(tomorrow.getDate() + 1)
+        const result = validateToken({
+            tokenExpireDate: () => tomorrow
+        })
+        expect(result).toBe(true);
+    });
+  
+    test("invalid token: token expired", () => {
+        const yesterday = new Date()
+        yesterday.setDate(yesterday.getDate() - 1)
+        const result = validateToken({
+            tokenExpireDate: () => yesterday
+        })
+      expect(result).toBe(false);
+    });
+});
