43e449094b77f6e6b982052e97a0680dfd686fef
Championrunner <27343592+Ishaan28malik@users.noreply.github.com>
2020-10-02 01:39:49 +0530

    Update IssueListItem.js

--- src/components/IssueListItem.js
+++ src/components/IssueListItem.js
@@ -8,7 +8,7 @@ import contrast from "contrast";
 
 dayjs.extend(relativeTime);
 
-function IssueListItem({title, labels, author, opened, type, participants, comments, milestone}) {
+function IssueListItem =({title, labels, author, opened, type, participants, comments, milestone})=> {
   const participantsDiffCount = 3;
   const participantsShowDiff = participants && participants.totalCount - participantsDiffCount;
   return (
