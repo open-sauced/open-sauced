052bd6880019378a54e9867819db1f422064a8c2
CodeItQuick <evanontario@hotmail.com>
2020-10-07 14:09:44 -0300

    improvements: some filtering, removed regex

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,20 +1,31 @@
 export function isValidRepoUrl(url) {
 
   try {
+    url = url.trim().toLowerCase();
+    url = url.substr(0, 1) === "/" ? url.substr(1) : url;
 
-    const isRelativeUrl = url.substr(0, 1) === "/";
+    const githubLink = "github.com/";
+    let newUrl;
 
-    if (isRelativeUrl)
-      url = "https://github.com" + url;
-    new URL(url);
-    const regex = new RegExp("https?://github.com/", "i"); // passing "i" as 2nd argument to ignore case
-    const relativeRepoUrl = url.toLowerCase().replace(regex, "");
+    const isRelativeUrl = url.substr(0, 4) !== "http";
+    if (isRelativeUrl) {
+      newUrl = "https://" + githubLink + url;
+      new URL(newUrl);
+
+      const [owner, repo] = url.split("/");
+      if (!owner || !repo) {
+        return [false, null];
+      }
+      return [true, url];
+    }
+    const getIndexGithub = url.toLowerCase().indexOf(githubLink) + githubLink.length; //?
+    const relativeRepoUrl = url.toLowerCase().substr(getIndexGithub, url.length);
 
     const [owner, repo] = relativeRepoUrl.split("/");
     if (!owner || !repo) {
       return [false, null];
     }
-    return [true, relativeRepoUrl];
+    return [true, owner + "/" + repo];
   } catch (_) {
     return [false, null];
   }
