0d1fd4fdc8a6963af5692933edcce3334a512f78
CodeItQuick <evanontario@hotmail.com>
2020-10-07 15:00:38 -0300

    refactored to reduce function complexity

--- src/lib/util.js
+++ src/lib/util.js
@@ -4,29 +4,43 @@ export function isValidRepoUrl(url) {
     url = url.trim().toLowerCase();
     url = url.substr(0, 1) === "/" ? url.substr(1) : url;
 
-    const githubLink = "github.com/";
-    let newUrl;
 
     const isRelativeUrl = url.substr(0, 4) !== "http";
     if (isRelativeUrl) {
-      newUrl = "https://" + githubLink + url;
-      new URL(newUrl);
-
-      const [owner, repo] = url.split("/");
-      if (!owner || !repo) {
-        return [false, null];
-      }
-      return [true, url];
+      return relativeUrlValidator(url);
     }
-    const getIndexGithub = url.toLowerCase().indexOf(githubLink) + githubLink.length; //?
-    const relativeRepoUrl = url.toLowerCase().substr(getIndexGithub, url.length);
+    return absoluteUrlValidator(url);
 
-    const [owner, repo] = relativeRepoUrl.split("/");
-    if (!owner || !repo) {
-      return [false, null];
-    }
-    return [true, owner + "/" + repo];
   } catch (_) {
     return [false, null];
   }
+}
+
+function relativeUrlValidator(url) {
+
+  const githubLink = "github.com/";
+
+  const newUrl = "https://" + githubLink + url;
+  new URL(newUrl);
+
+  const [owner, repo] = url.split("/");
+  if (!owner || !repo) {
+    return [false, null];
+  }
+  return [true, url];
+}
+
+function absoluteUrlValidator(url) {
+
+  const githubLink = "github.com/";
+  new URL(url);
+
+  const getIndexGithub = url.indexOf(githubLink) + githubLink.length; //?
+  const relativeRepoUrl = url.substr(getIndexGithub, url.length);
+
+  const [owner, repo] = relativeRepoUrl.split("/");
+  if (!owner || !repo) {
+    return [false, null];
+  }
+  return [true, owner + "/" + repo];
 }
