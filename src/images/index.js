456ab4374718ebcb6ee8684508b2cac93babde4b
Filip Troníček <filip.tronicek@seznam.cz>
2021-04-22 20:13:10 +0200

    Import GraphQL image

--- src/images/index.js
+++ src/images/index.js
@@ -5,6 +5,7 @@ import sauced from "./sauced.svg";
 import sauceFull from "./pizza-n-sauce.svg";
 import pizza from "./pizza.svg";
 import gucci from "./gucci.jpg";
+import graphql from "./graphql.png";
 import gitTrends from "./gitTrends.svg";
 import ohno from "./404.svg";
 import remirror from "./remirror.svg";
