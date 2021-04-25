b0758c415f33e1e77771aa2d7578d44173e1e7e3
Matthew Foley <mtfoley.mae@gmail.com>
2021-04-25 09:53:14 -0400

    fix: Remove unused imports, use Label import

--- stories/2-Card.stories.js
+++ stories/2-Card.stories.js
@@ -1,16 +1,15 @@
 import React from "react";
 import Card from "../src/components/Card";
 import Button, {InputButton} from "../src/styles/Button";
-import {AccentLink, TinyFont, MicroFont} from "../src/styles/Typography";
+import {AccentLink, MicroFont} from "../src/styles/Typography";
 import {PrimaryWithText} from "./1-Button.stories";
 import {CardPadding, ContextStyle, HintStyle, OnBoardStyle} from "../src/styles/Card";
 import Input from "../src/styles/Input";
 import Background from "../src/styles/Background";
 import TextArea from "../src/styles/TextArea";
-import {Flex, FlexCenter, FlexColumn, FloatLeft, IssuesColumn, SpaceAround, SpaceBetween} from "../src/styles/Grid";
+import {Flex, FlexCenter, FlexColumn, IssuesColumn, SpaceAround, SpaceBetween} from "../src/styles/Grid";
 import List from "../src/styles/List";
-import Avatar from "../src/styles/Avatar";
-import {chevronRight, check} from "../src/icons";
+import {check} from "../src/icons";
 import {PencilIcon} from "@primer/octicons-react";
 import RepoListItem from "../src/components/RepoListItem";
 import IssuesListItem from "../src/components/IssueListItem";
@@ -159,14 +158,14 @@ export const OnboardingCard = () => (
       <h4>Onboarding</h4>
       <div className="checkbox">
         <input id="agenda" type="checkbox" />
-        <label htmlFor="agenda">Follow the babel community</label>
+        <Label htmlFor="agenda">Follow the babel community</Label>
       </div>
       <div className="checkbox">
         <img src={check} alt="check" />
         <input id="agenda" type="checkbox" checked />
-        <label className="checked" htmlFor="agenda">
+        <Label className="checked" htmlFor="agenda">
           Onboarding
-        </label>
+        </Label>
       </div>
       <div className="checkbox">
         <input id="agenda" type="checkbox" />
