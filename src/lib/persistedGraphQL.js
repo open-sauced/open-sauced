dface0c4b3be0b6de3d70c27b80288ab54eeb845
Matthew Foley <mtfoley.mae@gmail.com>
2021-05-02 06:16:52 -0400

    refactor persisted query definitions

--- src/lib/persistedGraphQL.js
+++ src/lib/persistedGraphQL.js
@@ -1,6 +1,6 @@
 // This file is temporary and meant to be a workaround for GitHub App limitations
 // https://github.com/open-sauced/open-sauced/pull/699#issuecomment-649126860
-//
+
 const url = "https://serve.onegraph.com/graphql?app_id=06238984-0a96-4774-95ad-d7b654c980c5";
 
 // variables are generically labelled for easy updating.
@@ -14,7 +14,42 @@ const doc_id5 = "89a3dc22-4355-494e-9d6f-a29c21e065e0";
 const doc_id6 = "a510812e-ad0d-4181-bda3-805ee2481a83";
 
 // TODO: Move this entire file to an npm package
+function makeFetch(doc_id,requiredVariables=[],operationName=false){
+  return async function(variables=false){
+    let body = { doc_id };
+    if(operationName) body.operationName = operationName;
+    // Validate required variables by presence
+    if(requiredVariables.length > 0){
+      if(!variables || variables == undefined) throw "No required variables provided for persisted fetch";
+      let missing = [];
+      for(name of requiredVariables){
+        if(!variables.hasOwnProperty(name)) missing.push(name);
+      }
+      if(missing.length > 0) throw `Missing required variables: ${missing.join(", ")}.`
+    }
+    if(variables) body.variables = variables;
+    const options = {
+      method:"POST",
+      body: JSON.stringify(body)
+    };
+    const response = await fetch(url, options)
+    .then(res => res.json())
+    .then(json => json)
+    return response;
+  }
+}
 
+export {
+  persistedForkFetch: makeFetch(doc_id0,["repoName","repoOwner"],"FetchUserForkCount"),
+  persistedRepoDataFetch: makeFetch(doc_id1,["repo","owner"]),
+  persistedGoalFetch: makeFetch(doc_id2,["number"]),
+  persistedInteractionsFetch: makeFetch(doc_id3,["repo","owner"]),
+  persistedIssuesFetch: makeFetch(doc_id4,["repo","owner"]),
+  persistedDeploymentFetch: makeFetch(doc_id5),
+  persistedIssuesByLabelFetch: makeFetch(doc_id6,["repo","owner"]), //{repo: repo, owner: owner}
+};
+
+/*
 async function persistedForkFetch(repo, owner, queryName) {
   const options = {
     method: "POST",
@@ -98,13 +133,4 @@ async function persistedIssuesByLabelFetch(owner, repo) {
 
   return response;
 }
-
-export {
-  persistedForkFetch,
-  persistedDeploymentFetch,
-  persistedIssuesFetch,
-  persistedInteractionsFetch,
-  persistedGoalFetch,
-  persistedRepoDataFetch,
-  persistedIssuesByLabelFetch,
-};
+*/
