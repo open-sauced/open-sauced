c7aab94f29da16b4a1fbcbd290f9e22252e7619e
Matthew Foley <mtfoley.mae@gmail.com>
2021-06-07 17:40:45 -0400

    add margin to theme buttons

--- src/components/ThemeButtonGroup.js
+++ src/components/ThemeButtonGroup.js
@@ -8,8 +8,9 @@ import themeAuto from "../images/themeAuto.svg";
 function ThemeButtonGroup() {
   const [theme, setTheme] = useContext(ThemeContext);
   return (
-    <FlexCenter>
+    <FlexCenter style={{marginRight:"0.5rem"}}>
       <a
+        style={{margin:"0 .5rem"}}
         disabled={theme === "dark"}
         onClick={(event) => {
           event.preventDefault();
@@ -18,6 +19,7 @@ function ThemeButtonGroup() {
         <img src={darkMode} alt="dark mode" style={{padding:"0.25rem", borderRadius:"15%", backgroundColor:(theme === "dark") ? "#ccc" : "transparent"}}/>
       </a>
       <a
+        style={{margin:"0 .5rem"}}
         disabled={theme === "system"}
         onClick={(event) => {
           event.preventDefault();
@@ -26,6 +28,7 @@ function ThemeButtonGroup() {
         <img src={themeAuto} alt="theme auto" style={{padding:"0.25rem", borderRadius:"15%", backgroundColor:(theme === "system") ? "#ccc" : "transparent"}}/>
       </a>
       <a
+        style={{margin:"0 .5rem"}}
         disabled={theme === "light"}
         onClick={(event) => {
           event.preventDefault();
