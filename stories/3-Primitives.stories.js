16d9a9735436b08407b20f576e9805aa3f6d710a
Chad Stewart <chadrhonanstewart@gmail.com>
2021-06-11 11:32:33 -0500

    Updated button stories to button and theme stories to primitives in Storybook.

--- stories/3-Primitives.stories.js
+++ stories/3-Primitives.stories.js
@@ -1,21 +1,14 @@
 import React from "react";
 import Background from "../src/styles/Background";
-import Gradient from "../src/styles/Gradient";
-import Card from "../src/components/Card";
 import Avatar from "../src/styles/Avatar";
 import {face} from "../src/images";
+import darkMode from "../src/images/darkMode.svg";
+import lightMode from "../src/images/lightMode.svg";
+import themeAuto from "../src/images/themeAuto.svg";
 
 export default {
   title: "Primitives",
 };
-
-export const GradientBackground = () => (
-  <React.Fragment>
-    <Gradient>
-      <div style={{height: 1024, width: 498}} />
-    </Gradient>
-  </React.Fragment>
-);
 
 export const ProfileAvatar = () => (
   <Background style={{height: 1024, padding: "10px"}}>
@@ -23,36 +16,24 @@ export const ProfileAvatar = () => (
   </Background>
 );
 
-export const CardCommponent = () => (
-  <Background style={{height: 1024, padding: "10px"}}>
-    <Card>
-      <div style={{minHeight: 35}}>
-        <h1>Sample Card</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
-        </p>
-      </div>
-    </Card>
-  </Background>
-);
+export const DarkMode = () => (
+  <a
+    style={{margin:"0 .5rem"}}>
+      <img src={darkMode} alt="dark mode" style={{padding:"0.25rem", borderRadius:"15%"}}/>
+  </a>
+)
 
-export const FittedCardComponent = () => (
-  <Background style={{height: 1024, padding: "10px"}}>
-    <Card fitted>
-      <div style={{minHeight: 35}}>
-        <h1>Sample Card</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-          est laborum.
-        </p>
-      </div>
-    </Card>
-  </Background>
-);
+export const ThemeAuto = () => (
+  <a
+    style={{margin:"0 .5rem"}}>
+    <img src={themeAuto} alt="theme auto" style={{padding:"0.25rem", borderRadius:"15%", width: "1%", height: "1%"}}/>
+  </a>
+
+)
+
+export const LightMode = () => (
+  <a
+   style={{margin:"0 .5rem"}}>
+    <img src={lightMode} alt="light mode" style={{padding:"0.25rem", borderRadius:"15%"}} />
+  </a>
+)
