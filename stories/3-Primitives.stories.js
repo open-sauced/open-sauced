143e55f53c933628d54934ab1be17d8744c8d1a9
Chad Stewart <chadrhonanstewart@gmail.com>
2021-06-17 10:44:27 -0500

    Moved everything from Primitives to Miscellaneous. Will begin adding styles from Typography to Primitives category

--- stories/3-Primitives.stories.js
+++ stories/3-Primitives.stories.js
@@ -10,30 +10,8 @@ export default {
   title: "Primitives",
 };
 
-export const ProfileAvatar = () => (
+export const Placeholder = () => (
   <Background style={{height: 1024, padding: "10px"}}>
-    <Avatar style={{width: "40px"}} src={face} alt="profile pic" />
+    boo
   </Background>
 );
-
-export const DarkMode = () => (
-  <a
-    style={{margin:"0 .5rem"}}>
-      <img src={darkMode} alt="dark mode" style={{padding:"0.25rem", borderRadius:"15%"}}/>
-  </a>
-)
-
-export const ThemeAuto = () => (
-  <a
-    style={{margin:"0 .5rem"}}>
-    <img src={themeAuto} alt="theme auto" style={{padding:"0.25rem", borderRadius:"15%", width: "1%", height: "1%"}}/>
-  </a>
-
-)
-
-export const LightMode = () => (
-  <a
-   style={{margin:"0 .5rem"}}>
-    <img src={lightMode} alt="light mode" style={{padding:"0.25rem", borderRadius:"15%"}} />
-  </a>
-)
--- stories/7-Misc.stories.js
+++ stories/7-Misc.stories.js
@@ -1,4 +1,10 @@
+import React from "react";
 import Background from "../src/styles/Background";
+import Avatar from "../src/styles/Avatar";
+import {face} from "../src/images";
+import darkMode from "../src/images/darkMode.svg";
+import lightMode from "../src/images/lightMode.svg";
+import themeAuto from "../src/images/themeAuto.svg";
 import { ButtonBoard } from "../src/styles/Card";
 import { Flex } from "../src/styles/Grid";
 import { npm } from "../src/images";
@@ -6,6 +12,34 @@ import { npm } from "../src/images";
 export default {
     title: 'Miscellaneous'
 }
+
+export const ProfileAvatar = () => (
+    <Background style={{height: 1024, padding: "10px"}}>
+      <Avatar style={{width: "40px"}} src={face} alt="profile pic" />
+    </Background>
+  );
+  
+  export const DarkMode = () => (
+    <a
+      style={{margin:"0 .5rem"}}>
+        <img src={darkMode} alt="dark mode" style={{padding:"0.25rem", borderRadius:"15%"}}/>
+    </a>
+  )
+  
+  export const ThemeAuto = () => (
+    <a
+      style={{margin:"0 .5rem"}}>
+      <img src={themeAuto} alt="theme auto" style={{padding:"0.25rem", borderRadius:"15%", width: "1%", height: "1%"}}/>
+    </a>
+  
+  )
+  
+  export const LightMode = () => (
+    <a
+     style={{margin:"0 .5rem"}}>
+      <img src={lightMode} alt="light mode" style={{padding:"0.25rem", borderRadius:"15%"}} />
+    </a>
+  )
 
 export const Contributors = () => (
     <Background style={{height: 1024, padding: "10px"}}>
