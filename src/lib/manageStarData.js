f963651762971509640bf6fc8320b3e688e2374b
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2021-08-20 14:05:59 -0700

    figure out how to account for no stars.json

--- src/lib/manageStarData.js
+++ src/lib/manageStarData.js
@@ -1,13 +1,15 @@
 // remove duplicates from data based on existing stars
-function filterForUniqueStars(data, stars) {
+function filterForUniqueStars(data, stars || []) {
+  console.log(stars)
   return stars.filter(repo => {
     return data.find(repoData => repoData.full_name === repo.full_name) === undefined;
   });
 }
 
+// TODO: need to account for folks with no stars at all. 
 function remainingStars(data, stars) {
   const remainingStars = filterForUniqueStars(data, stars);
-  return [...remainingStars.slice(0, 3)];
+  return [...remainingStars.slice(0, 3)] || [] ;
 }
 
 export {remainingStars};
