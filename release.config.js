38c9e1c636cb716a263bab5fa5173862f4d0ff9c
TED Vortex <ted.vortex@gmail.com>
2021-10-03 18:09:34 +0200

    feat: testing diagram svg

--- release.config.js
+++ release.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  "extends": "@open-sauced/semantic-release-conventional-config"
+  "extends": "@open-sauced/semantic-release-conventional-config",
+  "plugins": [
+    ["@semantic-release/git", {
+      "assets": [
+        "CHANGELOG.md",
+        "package.json",
+        "npm-shrinkwrap.json",
+        "public/diagram.svg"
+      ],
+    }]
+  ]
 };
