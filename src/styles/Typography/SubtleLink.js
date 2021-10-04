2a1770f2ade02ed9fd3c4da3dd3b59de8aebbc68
Ilango <rajagopalilango@gmail.com>
2021-10-04 09:02:32 +0530

    Add dark mode color and update default color for SubtleLink

--- src/styles/Typography/SubtleLink.js
+++ src/styles/Typography/SubtleLink.js
@@ -2,11 +2,17 @@ import styled from "styled-components";
 import {fontSize, colors} from "../variables";
 
 const SubtleLink = styled.a`
-  color: ${colors.offWhite} !important;
+  color: ${colors.lightGrey} !important;
   text-decoration: none;
   font-weight: bold;
   font-size: ${fontSize.default};
   margin-right: 8px;
+  
+  body.dark & {
+    a: {
+      color: ${colors.lightestGrey} !important;
+    }
+  }
 `;
 
 export default SubtleLink;
