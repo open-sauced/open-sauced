0e50bc4a9672c179cf10ad5abacecdd8652b129c
mtfoley <mtfoley.mae@gmail.com>
2021-11-27 22:17:34 -0500

    integrate snowpack starter

---
+++ snowpack.config.mjs
@@ -0,0 +1,35 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+export default {
+    mount: {
+      public: { url: '/', static: true },
+      src: { url: '/dist' },
+    },
+    plugins: [
+      '@snowpack/plugin-react-refresh',
+      '@snowpack/plugin-dotenv',
+      [
+        '@snowpack/plugin-typescript',
+        {
+          /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+          ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+        },
+      ],
+    ],
+    routes: [
+      /* Enable an SPA Fallback in development: */
+      {"match": "routes", "src": ".*", "dest": "/index.html"},
+    ],
+    optimize: {
+      /* Example: Bundle your final build: */
+      // "bundle": true,
+    },
+    packageOptions: {
+      /* ... */
+    },
+    devOptions: {
+      port: 3000
+    },
+    buildOptions: {
+      /* ... */
+    },
+  };
