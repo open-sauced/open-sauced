1365a9922e5c7bf3f09583a43bd20db825223c6d
mtfoley <mtfoley.mae@gmail.com>
2021-11-27 22:39:11 -0500

    process.env -> import.meta

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -18,9 +18,9 @@ const isLocalhost = Boolean(
 );
 
 export default function register() {
-  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
+  if (import.meta.NODE_ENV === "production" && "serviceWorker" in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
+    const publicUrl = new URL(import.meta.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
       // from what our page is served on. This might happen if a CDN is used to
@@ -29,7 +29,7 @@ export default function register() {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = `${import.meta.PUBLIC_URL}/service-worker.js`;
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
