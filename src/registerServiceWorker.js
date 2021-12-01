92fa569558ecf51b3d5b022dac4ece7a7de663ac
mtfoley <mtfoley.mae@gmail.com>
2021-12-01 18:26:00 -0500

    try fixing service worker load

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -29,7 +29,7 @@ export default function register() {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = "/service-worker.js";
 
       if (!isLocalhost) {
         // Is not local host. Just register service worker
