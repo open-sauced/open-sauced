37e7af86cb328ed3896652b440296dfac75ab80d
Matthew <mtfoley.mae@gmail.com>
2021-12-01 11:14:33 -0500

    fix lint errors

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,5 +1,5 @@
-process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = "test";
 
 module.exports = {
-  plugins: [require('@snowpack/web-test-runner-plugin')()],
+  plugins: [require("@snowpack/web-test-runner-plugin")()],
 };
