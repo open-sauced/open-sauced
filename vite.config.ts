297af3ae9e5fb7ddd4e47330da8df20fb2fc6afd
mtfoley <mtfoley.mae@gmail.com>
2021-12-01 18:11:28 -0500

    target "build" for outDir

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    outDir: "build"
+  }
 })
