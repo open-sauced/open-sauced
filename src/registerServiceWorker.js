1052fbd1dd6cfc9cd169fc85bc47c8f241116a18
mtfoley <mtfoley.mae@gmail.com>
2021-12-09 16:59:03 -0500

    gut references to process.env

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -17,18 +17,11 @@ const isLocalhost = Boolean(
     window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/),
 );
 
-export default function register() {
-  if (process.env.NODE_ENV === "production" && "serviceWorker" in navigator) {
-    // The URL constructor is available in all browsers that support SW.
-    const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
-    if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
-      return;
-    }
-
+export default function registerSW() {
+  if ("serviceWorker" in navigator) {
     window.addEventListener("load", () => {
+      console.log('arguments',arguments);
+      console.log({isLocalhost})
       const swUrl = "/service-worker.js";
 
       if (!isLocalhost) {
