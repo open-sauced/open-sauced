a42a0deed57aa1a7218ba57c5ce024f002c56771
TED Vortex <ted.vortex@gmail.com>
2021-12-22 10:48:33 +0200

    feat: add dynamic templates and correct build options

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,26 @@ import react from "@vitejs/plugin-react";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
 import ViteLegacy from '@vitejs/plugin-legacy'
 import ViteVisualizer from "rollup-plugin-visualizer";
+import ViteHtml from 'vite-plugin-html'
 
+export default defineConfig(({command, mode, ...rest }) => {
+  // figure out commands
+  const isBuild = command === 'build';
 
-export default defineConfig(({ mode }) => {
+  // figure out modes
   const isDev = mode === "development";
   const isProd = mode === "production";
   const isReport = mode === "report";
+
+  // figure out custom build options
+  const isLegacy = process.env.VITE_LEGACY || false;
 
   const build = {
     outDir: "build",
     assetsDir: "static",
     sourcemap: !isDev,
+    rollupOptions: {},
     manifest: false,
-    rollupOptions: {}
   };
 
   const plugins = [
@@ -26,32 +33,36 @@ export default defineConfig(({ mode }) => {
       exclude: /\.stories\.(t|j)sx?$/,
       // Only .jsx files
       include: "**/*.jsx"
+    }),
+    ViteHtml({
+      minify: isBuild,
+      inject: {
+        data: {
+          title: `Open Sauced v${process.env.npm_package_version}`,
+        },
+      },
     })
   ];
 
   // broken until we figure out how to automate it w/wo workbox
-  if (isProd) {
-    // build.manifest = true;
+  // isProd && (build.manifest = true);
 
-    plugins.push(
-      ViteLegacy({
-        targets: [
-          'defaults',
-          'not IE 11'
-        ]
-      })
-    )
-  }
+  isBuild && isLegacy && plugins.push(
+    ViteLegacy({
+      targets: [
+        'defaults',
+        'not IE 11'
+      ]
+    })
+  )
 
-  if (isReport) {
-    plugins.push(
-      ViteVisualizer({
-        filename: "./build/bundle.html",
-        open: true,
-        gzipSize: true
-      })
-    );
-  }
+  isReport && plugins.push(
+    ViteVisualizer({
+      filename: "./build/bundle.html",
+      open: true,
+      gzipSize: true
+    })
+  );
 
   return {
     base: "/",
