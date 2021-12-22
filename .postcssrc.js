c9d4fe345c10b72c0eb185579d4b354ffb9b3fb4
TED Vortex <ted.vortex@gmail.com>
2021-12-22 12:47:41 +0200

    perf: reimplement postcss config from live build

---
+++ .postcssrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: {
+    'postcss-import': {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+    },
+    'postcss-normalize': {},
+  },
+};
