b9d2bea9f837013860f238351378300ef4bdd94e
TED Vortex <ted.vortex@gmail.com>
2021-12-22 07:34:34 +0200

    feat: add eslint vite plugin for hmr linting

--- src/index.jsx
+++ src/index.jsx
@@ -117,7 +117,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Index />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById( "root")
 );
 
 // If you want your app to work offline and load faster, you can change
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 // vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import eslintPlugin from "@nabla/vite-plugin-eslint";
 
 export default defineConfig({
   plugins: [
+    eslintPlugin(),
     react({
       // Exclude storybook stories
       exclude: /\.stories\.(t|j)sx?$/,
