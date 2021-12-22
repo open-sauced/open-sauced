b60f7dd700872fb34023380385f3c2c6eed6bdd7
TED Vortex <ted.vortex@gmail.com>
2021-12-22 09:37:32 +0200

    feat: normalize vite configuration to existing production opensauced.pizza webpack

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,60 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import eslintPlugin from "@nabla/vite-plugin-eslint";
+import ViteVisualizer from "rollup-plugin-visualizer";
 
-export default defineConfig({
-  plugins: [
+
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
+  const isProd = mode === "production";
+  const isReport = mode === "report";
+
+  const build = {
+    outDir: "build",
+    assetsDir: "static",
+    sourcemap: !isDev,
+    manifest: false,
+    rollupOptions: {}
+  };
+
+  const plugins = [
     eslintPlugin(),
     react({
       // Exclude storybook stories
       exclude: /\.stories\.(t|j)sx?$/,
       // Only .jsx files
-      include: '**/*.jsx',
+      include: "**/*.jsx"
     })
-  ],
-  // mode: "production",
-  build: {
-    outDir: "build"
+  ];
+
+  // broken until we figure out how to automate it w/wo workbox
+  if (isProd) {
+    // build.manifest = true;
   }
-})
+
+  if (isReport) {
+    plugins.push(
+      ViteVisualizer({
+        filename: "./build/bundle.html",
+        open: true,
+        gzipSize: true
+      })
+    );
+  }
+
+  return {
+    base: "/",
+    mode,
+    plugins,
+    publicDir: "public",
+    server: {
+      port: 3000,
+      open: true,
+    },
+    build,
+    preview: {
+      port: 3000,
+    },
+  };
+});
