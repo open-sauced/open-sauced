190c630852168ac8771fcc5a0822a429673c4e21
LevinGar <43618323+LevinGar@users.noreply.github.com>
2021-12-28 18:22:28 -0700

    Change Images url
    Requested change to marge PR that closes issue #1347

--- src/components/MetaDecorator.js
+++ src/components/MetaDecorator.js
@@ -15,7 +15,7 @@ function MetaDecorator({ title, description }) {
             //Open Graph Protocol Meta
                 <meta property="og:title" content={title} />
                 <meta property="og:description" content={description} />
-                <meta property="og:image" content="https://i.ibb.co/my2VqXw/sauced-social-card.png" />
+                <meta property="og:image" content="https://repository-images.githubusercontent.com/71359796/179eca00-061e-11eb-9b57-856a179db1a1" />
                 <meta property="og:url" content="https://opensauced.pizza/" />
                 <meta property="og:type" content="website" />
 
@@ -23,7 +23,7 @@ function MetaDecorator({ title, description }) {
                 <meta property="twitter:card" content="summary_large_image" />
                 <meta property="twitter:title" content={title} />
                 <meta property="twitter:description" content={description} />
-                <meta property="twitter:image" content="https://i.ibb.co/my2VqXw/sauced-social-card.png" />
+                <meta property="twitter:image" content="https://repository-images.githubusercontent.com/71359796/179eca00-061e-11eb-9b57-856a179db1a1" />
             </Helmet>
         </HelmetProvider>
     );
