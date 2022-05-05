9380c9581a145fcc82c74e8a13df545d8c68cbc6
Chad Stewart <chadrhonanstewart@gmail.com>
2022-05-05 07:49:09 -0500

    Tooltip is now animating properly.

--- src/styles/Tooltip.jsx
+++ src/styles/Tooltip.jsx
@@ -1,28 +1,29 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 import { TooltipContent, TooltipArrow } from "@radix-ui/react-tooltip";
+import { colors } from "./variables";
+
+const TooltipAnimation = keyframes`
+    0% { opacity: 0; transform: translateY(2px); }
+    100% { opacity: 1; transform: translateY(0); }
+`;
 
 const TooltipContainer = styled(TooltipContent)`
-    background-color: red;
+    background-color: ${colors.lightestGrey};
+    color: black;
     border-radius: 4px;
     padding: 15px;
     font-size: 15px;
     line-height: 1px;
-    @media (prefers-reduced-motion: no-preference) {
-        animation-duration: 400ms;
-        animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
-        animation-fill-mode: forwards;
-        will-change: transform, opacity;
-        &[data-state="delayed-open"] {
-          '&[data-side="top"]': { animationName: slideDownAndFade },
-          '&[data-side="right"]': { animationName: slideLeftAndFade },
-          '&[data-side="bottom"]': { animationName: slideUpAndFade },
-          '&[data-side="left"]': { animationName: slideRightAndFade },
-        },
+    animation-duration: 400ms;
+    animation-timing-function: cubic-bezier(0.16, 1, 0.3, 1);
+    animation-fill-mode: forwards;
+    &[data-state="delayed-open"] {
+        &[data-side="top"] { animation-name: ${TooltipAnimation}; }
     }
 `;
 
 const TooltipArrowComponent = styled(TooltipArrow)`
-    fill: red;
+    fill: ${colors.lightestGrey};
 `;
 
 export {
