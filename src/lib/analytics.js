248c74b512b53c0f136bc00d21b7d56452e8b7bf
Chad Stewart <chadrhonanstewart@gmail.com>
2022-05-13 19:52:39 -0500

    Apply fix to analytics to use env variables with vite.

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -1,7 +1,7 @@
 import posthog from "posthog-js";
 
 function initiatePostHog () {
-    posthog.init(import.meta.env.POSTHOG_ID, { api_host: 'https://app.posthog.com' });
+    posthog.init(import.meta.env.VITE_POSTHOG_ID, { api_host: 'https://app.posthog.com' });
     return;
 }
 
