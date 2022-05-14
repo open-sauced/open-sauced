ded5fae3f06539120e89c3e4f4e243c8e3fbcee3
TED Vortex <ted.vortex@gmail.com>
2022-05-15 00:33:41 +0200

    test: correct jest configuration and drop babel

--- .jest/setupTests.js
+++ .jest/setupTests.js
@@ -3,6 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import 'whatwg-fetch';
 import {configure} from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 
