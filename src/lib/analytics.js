ace492933caa57244384f6ff9822c9168cd8b06d
TED Vortex (Teodor-Eugen Duțulescu) <237133+0-vortex@users.noreply.github.com>
2022-05-16 01:26:18 +0200

    refactor: correct posthog analytics typo
    Co-authored-by: Brian Douglas <bdougie@users.noreply.github.com>

--- src/lib/analytics.js
+++ src/lib/analytics.js
@@ -3,7 +3,7 @@ import posthog from "posthog-js";
 const initiatePostHog = () =>
   posthog.init(import.meta.env.VITE_POSTHOG_API_KEY, { api_host: "https://app.posthog.com" });
 
-const capturePostHogAnayltics = (analyticsTitle, analyticsProperty, analyticsValue) => {
+const capturePostHogAnalytics = (analyticsTitle, analyticsProperty, analyticsValue) => {
   const propertyIsMissingOrIncorrectType = !(typeof analyticsTitle === "string" &&
     typeof analyticsProperty == "string" &&
     analyticsValue);
