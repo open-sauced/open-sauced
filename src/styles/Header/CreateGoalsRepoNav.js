43a0cadc3040cb7b3ba5ad5df63d7fa17e730653
Chad Stewart <chadrhonanstewart@gmail.com>
2022-05-22 16:05:56 -0500

    Changed add repo nav to be column at tablet and smaller screen sizes.

--- src/styles/Header/CreateGoalsRepoNav.js
+++ src/styles/Header/CreateGoalsRepoNav.js
@@ -1,9 +1,16 @@
 import styled from "styled-components";
+import MEDIA from "../mediaTemplates";
 
 const Container = styled.nav`
     display: flex;
     flex-direction: row;
     
+    ${MEDIA.TABLET`
+        flex-direction: column;
+        align-items: center;
+        padding-bottom: 10px;
+    `};
+
     * {
         padding-right: 10px;
     }
