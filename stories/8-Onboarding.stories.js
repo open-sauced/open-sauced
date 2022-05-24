507e047ff14108cf3e39c2ba6ffeddf63e82c26f
Chad Rhonan Stewart <chadrhonanstewart@gmail.com>
2022-05-24 09:47:39 -0400

    feat: Adds Repo suggestions to the onboarding flow (#1444)

--- stories/8-Onboarding.stories.js
+++ stories/8-Onboarding.stories.js
@@ -10,10 +10,31 @@ import {Tooltip, TooltipTrigger} from "@radix-ui/react-tooltip";
 import {TooltipContainer, TooltipArrowComponent} from "../src/styles/Tooltip";
 import {help} from "../src/icons";
 import {appInstall} from "../src/images";
+import { CreateGoalsRepoNav } from "../src/styles/Header";
+import RepositoryAvatar from "../src/styles/RepositoryAvatar";
+import {diary} from "../src/illustrations";
 
 export default {
     title: "Onboardring"
 };
+
+const repoInfo = [
+  {
+    repoOwner: "open-sauced",
+    repoName: "open-sauced",
+    repoDescription: "This is a project to identify your next open source contribution."
+  },
+  {
+    repoOwner: "npm",
+    repoName: "cli",
+    repoDescription: "the package manager for JavaScript"
+  },
+  {
+    repoOwner: "tailwindlabs",
+    repoName: "tailwindcss",
+    repoDescription: "A utility-first CSS framework for rapid UI development."
+  },
+];
 
 function CreateApp() {
   return (
@@ -35,9 +56,14 @@ function CreateApp() {
 }
 
  export function OnboardingFlow() {
+  const [selectedRepo, setSelectedRepo] = useState(0);
   const [installReady, setInstallReady] = useState(false);
+  const [databaseCreated, setDatabaseCreated] = useState(false);
   const _handleRepoCreation = () => {
     setInstallReady(prevState => true);
+  };
+  const _handleDatabaseCreation = () => {
+    setDatabaseCreated(prevState => true);
   };
 
   return (
@@ -48,7 +74,7 @@ function CreateApp() {
               <CreateApp />
           </SpaceBetween>
         </SpaceBetweenTop>
-        <Cards disabled={installReady}>
+        <Cards disabled={installReady || databaseCreated}>
           <SpaceBetween>
             <OnBoardingText>
               <h1>1</h1>
@@ -59,7 +85,7 @@ function CreateApp() {
             </Button>
           </SpaceBetween>
         </Cards>
-        <Cards disabled={!installReady}>
+        <Cards disabled={!installReady || databaseCreated}>
           <SpaceBetween>
             <OnBoardingText>
               <h1>2</h1>
@@ -80,21 +106,52 @@ function CreateApp() {
             <a
               rel="noreferrer"
               target="_blank">
-              <Button primary minWidth={175} maxWidth={175} disabled={!installReady}>Create database</Button>
+              <Button primary minWidth={175} maxWidth={175} disabled={!installReady} onClick={_handleDatabaseCreation}>Create database</Button>
             </a>
           </SpaceBetween>
         </Cards>
-        {/* TODO: issue #1428
-        
-        <Cards disabled={true}>
+        <Cards disabled={!databaseCreated}>
           <SpaceBetween>
             <OnBoardingText>
               <h1>3</h1>
               <p>And finally, it's time to follow some repos</p>
             </OnBoardingText>
-              <Button primary minWidth={175} disabled={true}>Add Repos</Button>
           </SpaceBetween>
-        </Cards> */}
+          {databaseCreated && (
+            <>
+              <CreateGoalsRepoNav>
+                {
+                  repoInfo.map((repo, index) => 
+                    <Button key={`repoNav${index + 1}`} onClick={() => setSelectedRepo(index)}>
+                      {`${repo.repoOwner}/${repo.repoName}`}
+                    </Button>
+                )}
+              </CreateGoalsRepoNav>
+              <SpaceBetweenTop>
+                <div>
+                  <a style={{textDecoration: "none"}} href={`https://github.com/${repoInfo[selectedRepo].repoOwner}/${repoInfo[selectedRepo].repoName}`} rel="noreferrer" target="_blank">
+                    <h1>
+                      <RepositoryAvatar alt="avatar" src={`https://avatars.githubusercontent.com/${repoInfo[selectedRepo].repoOwner}`} />
+                      {`${repoInfo[selectedRepo].repoOwner} / ${repoInfo[selectedRepo].repoName}`}
+                    </h1>
+                  </a>
+                    <p>{repoInfo[selectedRepo].repoDescription}</p>
+                  <small>
+                    <em>
+                      <a href="https://opensource.guide/how-to-contribute/" rel="noreferrer" target="_blank">
+                        Learn how to contribute to open source projects
+                      </a>
+                    </em>
+                  </small>
+                  <div style={{ paddingTop: 30 }}>
+                    <Button primary minWidth={175} >Add Repo"</Button>
+                  </div>
+                </div>
+                <Illustration className="productive-developer" alt="productive developer image" src={diary} />
+              </SpaceBetweenTop>
+            </>
+          )}
+        </Cards>
       </ContextStyle>
     </React.Fragment>
   );
