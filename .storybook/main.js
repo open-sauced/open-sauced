7135fe321593523eed47f80a645e3a95804e42e0
Nick Taylor <nick@iamdeveloper.com>
2022-11-21 11:12:41 -0500

    feat: enable vite for bundling Storybook (#1473)
    closes #1429

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,11 @@
-
 module.exports = {
-  "stories": [
-    "../stories/**/*.stories.js"
-  ],
-  "addons": [
+  core: { builder: "@storybook/builder-vite" },
+  stories: ["../stories/**/*.stories.jsx"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-storysource",
     "storybook-dark-mode",
   ],
-  "staticDirs": [
-    '../.storybook'
-  ]
-}
+  staticDirs: ["../.storybook"],
+};
