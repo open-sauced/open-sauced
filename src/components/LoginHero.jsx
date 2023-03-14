c3484e8f4f48b8c1c2d290c3d371ecc36d217ace
Akeem Oyebanji <oyebanjiakeem@gmail.com>
2023-03-14 20:33:21 +0100

    Update LoginHero.jsx

--- src/components/LoginHero.jsx
+++ src/components/LoginHero.jsx
@@ -10,7 +10,7 @@ function Hero({handleLogIn}) {
       <Wrapper>
         <Card>
           <FlexColumnCenter>
-            <h1>Open Sauced Dashboard and more pizza</h1>
+            <h1>Open Sauced and more pizza Dashboard</h1>
             <p>Login with GitHub to start tracking open source projects.</p>
             <br />
             <FlexCenter>
