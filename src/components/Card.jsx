310e0c31a3a7652813a3fae2bfd73221aa157b4a
Anush <anushshetty90@gmail.com>
2023-03-24 19:56:36 +0530

    fix: exported Cards as default

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,3 +4,5 @@ function Cards({fitted, disabled, children}) {
   return fitted ? <FittedCard data-testid="fitted-card">{children}</FittedCard> :
          disabled ? <DisabledCard data-testid="card">{children}</DisabledCard> : <Card data-testid="card">{children}</Card>;
 }
+
+export default Cards;
