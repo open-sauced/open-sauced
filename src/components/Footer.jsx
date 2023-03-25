852156fbae926a8e3006bdd9e81a1659fd6ced91
Debanjana-Ghosh <126650970+Debanjana-Ghosh@users.noreply.github.com>
2023-03-25 12:24:17 +0530

    added a smiley

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -37,7 +37,7 @@ function Footer() {
         <li className="p-2 sm:mr-5">
           <a href="https://www.dev.to/opensauced"
              target="_blank" rel="noreferrer"
-             aria-label="Visit Open Sauced GitHub Organisation">
+             aria-label="Visit Open Sauced GitHub Organisation :)">
             <FaDev className="text-offWhite hover:text-accent" />
           </a>
         </li>
