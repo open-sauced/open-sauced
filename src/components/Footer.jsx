ac78fdd3ac68e434e6ad64e0adc2b9c1dc3b38a3
Debanjana-Ghosh <126650970+Debanjana-Ghosh@users.noreply.github.com>
2023-03-25 12:26:33 +0530

    updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
         <li className="p-2 sm:mr-5">
           <a href="https://github.com/open-sauced/hot"
             target="_blank" rel="noreferrer"
-            aria-label="Visit Open Sauced GitHub Organisation">
+            aria-label="Visit Open Sauced GitHub Organisation, visit us!">
             <FaGithub className="text-offWhite hover:text-accent" />
           </a>
         </li>
